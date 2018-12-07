61766842c12570ecb21aae23d69bf7eba8a7a441
NDMurray <nicholasdmurray@gmail.com>
2018-12-07 15:14:16 -0500

    preliminary bootstrap template

--- app/movie_db/views.py
+++ app/movie_db/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
 
-# Create your views here.
+from .models import Actor
+from .models import ActorLookup	
+from .models import Director
+from .models import DirectorLookup
+from .models import Writer
+from .models import WriterLookup
+from .models import Title
+
+def index(request):
+	return HttpResponse("Welcome. To the Movie DB")
+
+class HomePageView(generic.TemplateView):
+	template_name = 'movie_db/home.html'
--- app/mysite/urls.py
+++ app/mysite/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.http import HttpResponseRedirect
+from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda r: HttpResponseRedirect('movie_db/')),
     path('admin/', admin.site.urls),
-]
+    path('movie_db/', include('movie_db.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
