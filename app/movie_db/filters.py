a21d6123d5f2e02909c46ae8900af7b0c4c7a7a0
NDMurray <nicholasdmurray@gmail.com>
2018-12-08 16:37:19 -0500

    front-end and search form

--- app/movie_db/filters.py
+++ app/movie_db/filters.py
@@ -16,13 +16,50 @@ class TitleFilter(django_filters.FilterSet):
 		lookup_expr='icontains'
 	)
 
-	rating = django_filters.NumericRangeFilter (
-		field_name = 'averageRating',
-		label = "IMDB Rating (Avg)",
-		lookup_expr = 'icontains'
-
+	genres = django_filters.CharFilter(
+		field_name='genres',
+		label='Genre(s)',
+		lookup_expr='icontains'
 	)
 
+	actors = django_filters.CharFilter(
+		field_name='actorlookup__actor__primaryname',
+		label='Actor(s)',
+		lookup_expr='icontains'
+	)
+
+	directors = django_filters.CharFilter(
+		field_name='directorlookup__director__primaryname',
+		label='Director(s)',
+		lookup_expr='icontains'
+	)
+
+	writers = django_filters.CharFilter(
+		field_name='writerlookup__writer__primaryname',
+		label='Writer(s)',
+		lookup_expr='icontains'
+	)
+
+	class Meta:
+		model = Title
+		# form = SearchForm
+		# fields [] is required, even if empty.
+		fields = []
+
+# class ActorFilter(django_filters.FilterSet):		
+# 	actor = django_filters.CharFilter(
+# 		field_name='primaryname',
+# 		label='Actor(s)',
+# 		lookup_expr='icontains'
+# 	)
+
+# 	class Meta:
+# 		model = Actor
+# 		# form = SearchForm
+# 		# fields [] is required, even if empty.
+# 		fields = []
+
+	
 	# Add description, heritage_site_category, region, sub_region and intermediate_region filters here
 
 	# country_area = django_filters.ModelChoiceFilter(
--- app/movie_db/forms.py
+++ app/movie_db/forms.py
@@ -14,3 +14,14 @@ class TitleForm(forms.ModelForm):
 		self.helper = FormHelper()
 		self.helper.form_method = 'post'
 		self.helper.add_input(Submit('submit', 'submit'))
+
+# class ActorForm(forms.ModelForm):
+# 	class Meta:
+# 		model = Actor
+# 		fields = '__all__'
+
+# 	def __init__(self, *args, **kwargs):
+# 		super().__init__(*args, **kwargs)
+# 		self.helper = FormHelper()
+# 		self.helper.form_method = 'post'
+# 		self.helper.add_input(Submit('submit', 'submit'))
